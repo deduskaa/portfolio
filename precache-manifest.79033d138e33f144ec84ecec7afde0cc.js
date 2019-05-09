0b419d56fdee153cfcb8ff01fb3a27473b292422
deduskaa <jennny.tran@gmail.com>
2019-05-09 16:24:39 +0300

    Updates

---
+++ precache-manifest.79033d138e33f144ec84ecec7afde0cc.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05479222e9b903a4c9e28e4df2dd45c4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "514cba4a4d20c588267a",
+    "url": "/portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0cf600afba632858d279",
+    "url": "/portfolio/static/css/main.ad3efbc9.chunk.css"
+  },
+  {
+    "revision": "514cba4a4d20c588267a",
+    "url": "/portfolio/static/js/2.691ce006.chunk.js"
+  },
+  {
+    "revision": "0cf600afba632858d279",
+    "url": "/portfolio/static/js/main.c15f20a2.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "8664753d92ff30fedfffac3df858a865",
+    "url": "/portfolio/static/media/companysearch_companypage.8664753d.jpg"
+  },
+  {
+    "revision": "800c6d4c3807f5459c2f1fb1bfd187ca",
+    "url": "/portfolio/static/media/companysearch_frontpage.800c6d4c.jpg"
+  },
+  {
+    "revision": "df7e6594f21cbc71e2fdc08f6673308a",
+    "url": "/portfolio/static/media/companysearch_performers.df7e6594.jpg"
+  },
+  {
+    "revision": "82189696539164bc0b0e271a3bcac397",
+    "url": "/portfolio/static/media/envelope-solid.82189696.svg"
+  },
+  {
+    "revision": "f5a069d24c7855d24188c14f570b6a57",
+    "url": "/portfolio/static/media/github.f5a069d2.svg"
+  },
+  {
+    "revision": "6f7f616f44e4c4244580338661f876d9",
+    "url": "/portfolio/static/media/instagram.6f7f616f.svg"
+  },
+  {
+    "revision": "c833257be219947d273fd85a736c32e9",
+    "url": "/portfolio/static/media/krh.c833257b.jpg"
+  },
+  {
+    "revision": "a284f8bb927825b98a2fa0bc94315ad2",
+    "url": "/portfolio/static/media/krh_desktop.a284f8bb.jpg"
+  },
+  {
+    "revision": "695e9d9b99272455c7745fcbd0ff1fa3",
+    "url": "/portfolio/static/media/krh_desktop_mobile.695e9d9b.jpg"
+  },
+  {
+    "revision": "419156cde40c54976523e4262504f3fe",
+    "url": "/portfolio/static/media/krh_frontpage.419156cd.jpg"
+  },
+  {
+    "revision": "48a2133dd5c34ad46cc6ab6ff06c03ec",
+    "url": "/portfolio/static/media/linkedin.48a2133d.svg"
+  },
+  {
+    "revision": "829343bd25e7883578dfe6d9f6125f2d",
+    "url": "/portfolio/static/media/marketing.829343bd.jpg"
+  },
+  {
+    "revision": "6850f69530dea1874b5ea142aee107e8",
+    "url": "/portfolio/static/media/marketing_rollup.6850f695.jpg"
+  },
+  {
+    "revision": "19b149a83f7ee904a94fb234bf6051f1",
+    "url": "/portfolio/static/media/marketing_social.19b149a8.jpg"
+  },
+  {
+    "revision": "9c2e6b5944c9f1bf632d70686989205f",
+    "url": "/portfolio/static/media/movix.9c2e6b59.jpg"
+  },
+  {
+    "revision": "0cc4528dca8f4d1dfdf5db7a89d27916",
+    "url": "/portfolio/static/media/movix_frontpage.0cc4528d.jpg"
+  },
+  {
+    "revision": "2cdf3f6a454b829a7acd3ca8530acd5b",
+    "url": "/portfolio/static/media/movix_moviepage.2cdf3f6a.jpg"
+  },
+  {
+    "revision": "a4da0d4bfcc21dbb447bbfecf6f45290",
+    "url": "/portfolio/static/media/movix_search.a4da0d4b.jpg"
+  },
+  {
+    "revision": "db6d8f0729c0ea9747c149d3d2b3583d",
+    "url": "/portfolio/static/media/youngme.db6d8f07.jpg"
+  },
+  {
+    "revision": "7ec7f4a12b3e34c578ebe62ac655603b",
+    "url": "/portfolio/static/media/yrityshaku.7ec7f4a1.jpg"
+  }
+]);
